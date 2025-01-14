d7ef363d87903fbcceae37635ad03680f211d2da
Tseng <tseng7510@gmail.com>
2025-01-14 13:58:21 +0800

    20250114更新

--- include/Footer.js
+++ include/Footer.js
@@ -7,11 +7,12 @@ const FooterContent = `
             地址：24220 新北市新莊區中平路439號北棟14F/15F/16F<br />
             總機：02-89953456 / 89953457 / 899534
             <p>Copyright© 2024 原住民族委員會 All Rights Reserved.</p>
-            <!-- footerTextBlock 增加 break 避免換行 -->
-            <div class="footerTextBlock">
-              <a href="#"><div className="pic"><img src="images/n1.png" alt=""></div>圖片+文字連結</a>
+            <!-- p 增加 noBreak 避免換行 -->
+            <p class="noBreak">
+              <span>純文字純文</span>
+              <a href="#"><img src="images/n1.png" alt=""> 圖片+文字連結</a>
               <a href="#">文字連結</a>
-            </div>
+            </p>
           </div>
         </div>
       </footer>`;
